58de0f330b6864ac42a7959ad722a9412c4b9699
Olivier Davignon <davignon@cern.ch>
2016-08-02 19:06:21 +0200

    Switch order of variables in tau LUTs, transparent to user

--- L1Trigger/L1TNtuples/python/L1NtupleAOD_cff.py
+++ L1Trigger/L1TNtuples/python/L1NtupleAOD_cff.py
@@ -5,8 +5,8 @@ from L1Trigger.L1TNtuples.l1RecoTree_cfi import *
 from L1Trigger.L1TNtuples.l1JetRecoTree_cfi import *
 from L1Trigger.L1TNtuples.l1MetFilterRecoTree_cfi import *
 from L1Trigger.L1TNtuples.l1ElectronRecoTree_cfi import *
-from L1Trigger.L1TNtuples.l1TauRecoTree_cfi import *
-#from L1Trigger.L1TNtuples.l1TauRecoTree_2015_cfi import *
+#from L1Trigger.L1TNtuples.l1TauRecoTree_cfi import *
+from L1Trigger.L1TNtuples.l1TauRecoTree_2015_cfi import *
 from L1Trigger.L1TNtuples.l1MuonRecoTree_cfi import *
 
 L1NtupleAOD = cms.Sequence(
